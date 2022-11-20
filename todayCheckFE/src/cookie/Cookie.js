151b2dcd4fd595f58c5b11beffbc76bca02f1b4b
kimwanyoung <dhks2869@gmail.com>
2022-11-20 16:26:36 +0900

    ADD:OAuth Login

---
+++ todayCheckFE/src/cookie/Cookie.js
@@ -0,0 +1,37 @@
+import { Cookies } from 'react-cookie';
+
+const cookies = new Cookies();
+
+export const setRefreshToken = refreshToken => {
+  const today = new Date();
+  const expireDate = today.setDate(today.getDate() + 30);
+
+  return cookies.set('refreshToken', refreshToken, {
+    expires: new Date(expireDate),
+    sameSite: 'strict',
+    path: '/',
+  });
+};
+
+export const setAccessToken = accessToken => {
+  const today = new Date();
+  const expireDate = today.setDate(today.getDate() + 5);
+
+  return cookies.set('accessToken', accessToken, {
+    expires: new Date(expireDate),
+    sameSite: 'strict',
+    path: '/',
+  });
+};
+
+export const getAccessToken = () => {
+  return cookies.get('accessToken');
+};
+
+export const getRefreshToken = () => {
+  return cookies.get('refreshToken');
+};
+
+export const removeRefreshToken = () => {
+  return cookies.remove('refreshToken', { sameSite: 'strict', path: '/' });
+};
