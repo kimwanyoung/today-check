ca36682d41c4f00d5adbb8a8d4398ec620949bf7
5tr1ker <follow2272@naver.com>
2022-12-10 00:02:17 +0900

    Merge branch 'develop' of https://github.com/5tr1ker/today-check into develop
    # Conflicts:
    #	todayCheck/.gradle/7.5/executionHistory/executionHistory.lock
    #	todayCheck/.gradle/7.5/fileHashes/fileHashes.lock
    #	todayCheck/.gradle/buildOutputCleanup/buildOutputCleanup.lock
    #	todayCheck/.settings/org.eclipse.buildship.core.prefs
    #	todayCheck/lib/bin/main/com/team/todaycheck/main/WebSecurityConfig.class
    #	todayCheck/lib/bin/main/com/team/todaycheck/main/service/PostService.class
    #	todayCheck/lib/bin/test/com/team/todaycheck/main/PostTest.class
    #	todayCheck/lib/src/main/java/com/team/todaycheck/main/service/PostService.java

---
+++ todayCheckFE/src/pages/PostingPage/PostingDetail.js
@@ -0,0 +1,149 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+import { useLocation } from 'react-router-dom';
+import { FaRegThumbsUp } from 'react-icons/fa';
+
+const PostingDetail = () => {
+  const [comment, setComment] = useState('');
+  const [commentList, setCommentList] = useState([]);
+  const location = useLocation();
+  console.log(location.state);
+
+  const handleComment = e => {
+    setComment(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setCommentList(prev => {
+      return [...prev, comment];
+    });
+    setComment('');
+  };
+  return (
+    <DetailWrapper>
+      <DetailContentWrapper>
+        <Title>
+          <p>{location.state.title}</p>
+          <Date>{location.state.date}</Date>
+        </Title>
+        <PostingImage
+          src={`data:image/;base64,${location.state.image.body}`}
+          alt="post imag"
+        />
+        <UserInfo>
+          <Name>글 작성자 : {location.state.writer}</Name>
+          <Name>
+            <Thumbs /> 추천 수 : {location.state.recommendation}
+          </Name>
+        </UserInfo>
+        <Desc>
+          <p>{location.state.description}</p>
+        </Desc>
+        <Comment onSubmit={handleSubmit}>
+          <CommentBox>
+            {commentList.map((prop, idx) => (
+              <CommentContent key={idx}>{prop}</CommentContent>
+            ))}
+          </CommentBox>
+          <CommentInput onChange={handleComment} value={comment} />
+        </Comment>
+      </DetailContentWrapper>
+    </DetailWrapper>
+  );
+};
+
+export default PostingDetail;
+
+const DetailWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+  background-color: #eeeeff;
+  overflow-y: scroll;
+`;
+
+const DetailContentWrapper = styled.div`
+  width: 40rem;
+  background-color: #fefefe;
+`;
+
+const Title = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 4rem;
+  padding-left: 2rem;
+  padding-right: 2rem;
+  font-size: 1.5rem;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+`;
+
+const Date = styled.p`
+  font-size: 0.9rem;
+  color: grey;
+`;
+
+const PostingImage = styled.img`
+  width: 100%;
+  height: 20rem;
+  padding-bottom: 2rem;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+`;
+
+const UserInfo = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 3rem;
+  padding: 0 1rem 0 1rem;
+`;
+
+const Name = styled.p`
+  font-size: 1rem;
+  color: black;
+`;
+
+const Thumbs = styled(FaRegThumbsUp)``;
+
+const Desc = styled.div`
+  width: 100%;
+  height: 3rem;
+  padding: 0 1rem 0 1rem;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+`;
+
+const Comment = styled.form`
+  position: relative;
+  width: 100%;
+  height: 30rem;
+  background-color: white;
+`;
+
+const CommentInput = styled.input`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 3rem;
+  font-size: 1rem;
+  text-indent: 1rem;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+`;
+
+const CommentBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 80%;
+`;
+
+const CommentContent = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 2rem;
+  padding-left: 1rem;
+`;
